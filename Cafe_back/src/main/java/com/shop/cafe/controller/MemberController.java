bb8634094c8084b78618e4396d12fc06b7b2c2e8
seungwoo <zoozoo1302@gmail.com>
2025-03-05 16:35:01 +0900

    DS_STORE 제거

--- Cafe_back/src/main/java/com/shop/cafe/controller/MemberController.java
+++ Cafe_back/src/main/java/com/shop/cafe/controller/MemberController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
+	
+	@GetMapping("login")
+	public String login() {
+		return "ok";
+	}
 	
 	@PostMapping("insertMember")
 	public Map<String, String> insertMember(@RequestBody Member m) {
